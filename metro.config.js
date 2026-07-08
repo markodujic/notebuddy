138cc0538ba96cdd92f7d09a0d8e9a04104c4a97
Marko <info@klavierlehrer-koeln.de>
2026-07-08 10:50:25 +0200

    chore: add VS Code debugging setup with REACT_EDITOR integration

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+// Tell React Native Element Inspector to open source files in VS Code
+process.env.REACT_EDITOR = process.env.REACT_EDITOR || 'code';
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
